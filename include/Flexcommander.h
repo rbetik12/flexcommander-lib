9470f038d67b5a7f96cf02f963a3dc3b14cdd3a9
Vitaliy <mr.prikota@gmail.com>
2021-03-25 11:31:38 +0300

    Added export macros

--- include/Flexcommander.h
+++ include/Flexcommander.h
@@ -4,6 +4,21 @@
 #include <blkid/blkid.h>
 #include "HFSPlus.h"
 
+#if defined(_MSC_VER)
+//  Microsoft
+    #define EXPORT __declspec(dllexport)
+    #define IMPORT __declspec(dllimport)
+#elif defined(__GNUC__)
+//  GCC
+#define EXPORT __attribute__((visibility("default")))
+#define IMPORT
+#else
+//  do nothing and hope for the best?
+    #define EXPORT
+    #define IMPORT
+    #pragma warning Unknown dynamic link import/export semantics.
+#endif
+
 typedef struct FlexCommanderProbeInfo {
     blkid_cache blkCache;
 } FlexCommanderProbeInfo;
@@ -16,9 +31,9 @@ typedef struct FlexCommanderFS {
 } FlexCommanderFS;
 
 //Functions for block device probing
-int Init(FlexCommanderProbeInfo* info);
-int ProbeDevices(FlexCommanderProbeInfo* info);
-int IterateDevices(FlexCommanderProbeInfo* info);
+int EXPORT Init(FlexCommanderProbeInfo* info);
+int EXPORT ProbeDevices(FlexCommanderProbeInfo* info);
+int EXPORT IterateDevices(FlexCommanderProbeInfo* info);
 
 //Functions for filesystem interaction
 int FlexOpen(const char * path, FlexCommanderFS* fs);
